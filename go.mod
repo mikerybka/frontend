dd633f86e18f2a67dd75357a0a6d9380a28e2a2d
Mike Rybka <merybka@gmail.com>
2025-02-28 14:11:03 -0500

    Change from lib to cmd

--- go.mod
+++ go.mod
@@ -1,3 +1,19 @@
 module github.com/mikerybka/frontend
 
 go 1.24.0
+
+require (
+	github.com/mikerybka/twilio v0.0.0-20250118185923-a37772540481
+	github.com/mikerybka/util v0.0.0-20250128121415-eb8cad79f499
+	golang.org/x/crypto v0.35.0
+)
+
+require (
+	github.com/fsnotify/fsnotify v1.8.0 // indirect
+	github.com/kr/fs v0.1.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/pkg/sftp v1.13.7 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+
+	"github.com/mikerybka/twilio"
+	"github.com/mikerybka/util"
+	"golang.org/x/crypto/acme/autocert"
+)
+
+func main() {
+	// Fetch certDir from env var
+	certDir := os.Getenv("CERT_DIR")
+	if certDir == "" {
+		log.Fatal("CERT_DIR environment variable is not set")
+	}
+
+	// Fetch BACKEND_URL from environment variable
+	backendURL := os.Getenv("BACKEND_URL")
+	if backendURL == "" {
+		log.Fatal("BACKEND_URL environment variable is not set")
+	}
+
+	// Parse the backend URL
+	parsedURL, err := url.Parse(backendURL)
+	if err != nil {
+		log.Fatalf("Invalid BACKEND_URL: %v", err)
+	}
+
+	// Create a certificate manager
+	certManager := autocert.Manager{
+		Cache:  autocert.DirCache(certDir),
+		Prompt: autocert.AcceptTOS,
+		HostPolicy: func(ctx context.Context, host string) error {
+			fmt.Println(host)
+			return nil
+		},
+	}
+
+	// Create the HTTP handler
+	h := &Server{
+		TwilioClient: &twilio.Client{
+			AccountSID:  util.RequireEnvVar("TWILIO_ACCOUNT_SID"),
+			AuthToken:   util.RequireEnvVar("TWILIO_AUTH_TOKEN"),
+			PhoneNumber: util.RequireEnvVar("TWILIO_PHONE_NUMBER"),
+		},
+		AdminPhone: util.RequireEnvVar("ADMIN_PHONE"),
+		BackendURL: parsedURL,
+	}
+
+	// Create an HTTPS server using autocert
+	httpsServer := &http.Server{
+		Addr:      ":443",
+		Handler:   h,
+		TLSConfig: certManager.TLSConfig(),
+	}
+
+	// Redirect HTTP to HTTPS
+	go func() {
+		http.ListenAndServe(":80", certManager.HTTPHandler(nil))
+	}()
+
+	// Start the HTTPS server
+	log.Println("Starting server on :443")
+	if err := httpsServer.ListenAndServeTLS("", ""); err != nil {
+		log.Fatalf("Failed to start HTTPS server: %v", err)
+	}
+}
+
+type Server struct {
+	TwilioClient *twilio.Client
+	AdminPhone   string
+	BackendURL   *url.URL
+	error502s    int
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	proxy := httputil.NewSingleHostReverseProxy(s.BackendURL)
+	proxy.ModifyResponse = func(r *http.Response) error {
+		// Notify the admin if the server goes down
+		if r.StatusCode == http.StatusBadGateway {
+			s.error502s++
+			switch s.error502s {
+			case 1:
+				s.TwilioClient.SendSMS(s.AdminPhone, "502 from backend")
+			case 10:
+				s.TwilioClient.SendSMS(s.AdminPhone, "Ten 502s")
+			case 100:
+				s.TwilioClient.SendSMS(s.AdminPhone, "A hundred 502s")
+			case 1000:
+				s.TwilioClient.SendSMS(s.AdminPhone, "A thousand 502s")
+			case 1_000_000:
+				s.TwilioClient.SendSMS(s.AdminPhone, "A million 502s")
+			case 1_000_000_000:
+				s.TwilioClient.SendSMS(s.AdminPhone, "A billion 502s")
+			case 1_000_000_000_000:
+				s.TwilioClient.SendSMS(s.AdminPhone, "A trillion 502s")
+			}
+		}
+		return nil
+	}
+	proxy.ServeHTTP(w, r)
+}
