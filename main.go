05e00fa7aaf0d2e2d6ae00025ff073d052cf479d
Mike Rybka <merybka@gmail.com>
2025-03-01 11:07:21 -0500

    Fix 502 notification bug

--- main.go
+++ main.go
@@ -83,29 +83,38 @@ type Server struct {
 }
 
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	wr := &StatusCodeRecorder{ResponseWriter: w}
+
 	proxy := httputil.NewSingleHostReverseProxy(s.BackendURL)
-	proxy.ModifyResponse = func(r *http.Response) error {
-		// Notify the admin if the server goes down
-		if r.StatusCode == http.StatusBadGateway {
-			s.error502s++
-			switch s.error502s {
-			case 1:
-				s.TwilioClient.SendSMS(s.AdminPhone, "502 from backend")
-			case 10:
-				s.TwilioClient.SendSMS(s.AdminPhone, "Ten 502s")
-			case 100:
-				s.TwilioClient.SendSMS(s.AdminPhone, "A hundred 502s")
-			case 1000:
-				s.TwilioClient.SendSMS(s.AdminPhone, "A thousand 502s")
-			case 1_000_000:
-				s.TwilioClient.SendSMS(s.AdminPhone, "A million 502s")
-			case 1_000_000_000:
-				s.TwilioClient.SendSMS(s.AdminPhone, "A billion 502s")
-			case 1_000_000_000_000:
-				s.TwilioClient.SendSMS(s.AdminPhone, "A trillion 502s")
-			}
+	proxy.ServeHTTP(wr, r)
+
+	if wr.StatusCode == http.StatusBadGateway {
+		s.error502s++
+		switch s.error502s {
+		case 1:
+			s.TwilioClient.SendSMS(s.AdminPhone, "502 from backend")
+		case 10:
+			s.TwilioClient.SendSMS(s.AdminPhone, "Ten 502s")
+		case 100:
+			s.TwilioClient.SendSMS(s.AdminPhone, "A hundred 502s")
+		case 1000:
+			s.TwilioClient.SendSMS(s.AdminPhone, "A thousand 502s")
+		case 1_000_000:
+			s.TwilioClient.SendSMS(s.AdminPhone, "A million 502s")
+		case 1_000_000_000:
+			s.TwilioClient.SendSMS(s.AdminPhone, "A billion 502s")
+		case 1_000_000_000_000:
+			s.TwilioClient.SendSMS(s.AdminPhone, "A trillion 502s")
 		}
-		return nil
 	}
-	proxy.ServeHTTP(w, r)
+}
+
+type StatusCodeRecorder struct {
+	http.ResponseWriter
+	StatusCode int
+}
+
+func (rw *StatusCodeRecorder) WriteHeader(code int) {
+	rw.StatusCode = code
+	rw.ResponseWriter.WriteHeader(code)
 }
